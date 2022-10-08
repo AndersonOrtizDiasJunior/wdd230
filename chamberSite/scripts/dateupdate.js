05d481bac3ba986434b98ed8e15b1fcc7686cccc
AndersonOrtizDiasJunior <anderson_ortiz@hotmail.com.br>
2022-10-07 22:44:47 -0300

    it is almost midnight, i hate my life

---
+++ chamberSite/scripts/dateupdate.js
@@ -0,0 +1,6 @@
+let yearSpan = document.querySelector("#year")
+let currentYear = new Date().getFullYear()
+yearSpan.textContent = `© ${currentYear} .:|:. Anderson Ortiz Dias Junior .:|:. Brazil`
+
+let updateSpan = document.querySelector("#lastModfied")
+updateSpan.textContent  = `Last updated ${document.lastModified} (UTC-3)`
