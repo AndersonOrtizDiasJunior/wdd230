8b54e647ca0db478f9e7132c9f054752e8f3c512
AndersonOrtizDiasJunior <anderson_ortiz@hotmail.com.br>
2022-10-10 07:30:30 -0300

    feat update home page

--- chamberSite/scripts/responsive-menu.js
+++ chamberSite/scripts/responsive-menu.js
@@ -1,40 +1,3 @@
-class ResponsiveMenu extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = `            <nav class="navigation">
-        <div class="header">                      
-        <img class="navLogo" src="images/mineraltownsmalllogo.png" width="50px" height="50px" alt="chamber logo">
-        <a class="name" href="index.html">
-            <span class="chamberName">Friends of Mineral Town</span>
-            <br>
-            Chamber of commerce
-        </a>
-        <ul>
-            <li><a href="discover.html">Discover</a></li>
-            <li><a href="directory.html">Directory</a></li>
-            <li><a href="join.html">Join</a></li>
-            <li><a href="contact.html">Contact</a></li>
-        </ul>
-        <div class="socialMedia">
-            <a href="https://www.facebook.com/">
-                <img src="images/facebook-logo.png" width="20px" height="20px" alt="facebook logo">
-            </a>
-            <a href="https://www.linkedin.com/">
-                <img src="images/linkedin-logo.png" width="20px" height="20px" alt="linkedin logo">
-            </a>
-        </div>
-        <a class="ham" href="#"></a>
-        <span class="today">Error loading date</span>
-    </div>
-        <ul class="togglableLinks">
-            <li><a href="index.html">Home</a></li>
-            <li><a href="discover.html">Discover</a></li>
-            <li><a href="directory.html">Directory</a></li>
-            <li><a href="join.html">Join</a></li>
-            <li><a href="contact.html">Contact</a></li>
-        </ul>
-    </nav>`
-    }
-}
 function weekDay() {
     var date = new Date();
     var weekdays = new Array(7);
@@ -48,7 +11,6 @@ function weekDay() {
    return weekdays[date.getDay()];
 }
 
-customElements.define('responsive-menu', ResponsiveMenu);
 
 const hambutton = document.querySelector('.ham');
 const mainnav = document.querySelector('.navigation')
@@ -61,3 +23,8 @@ const today = document.querySelector(".today")
 let unformatedDate = new Date();
 let month = unformatedDate.toLocaleString('default', { month: 'long' })
 today.textContent = `${weekDay()}, ${unformatedDate.getDate()} ${month} ${unformatedDate.getFullYear()}`
+
+let meeting = document.querySelector(".meetingCall");
+if (( weekDay() == "Monday") || (weekDay() == "Tuesday" )) {
+    meeting.style.display = "block";
+}
