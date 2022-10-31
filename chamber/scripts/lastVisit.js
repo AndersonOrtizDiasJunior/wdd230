ea545a5580b2ae002afa3073577c01062fa923be
AndersonOrtizDiasJunior <anderson_ortiz@hotmail.com.br>
2022-10-31 13:57:21 -0300

    FEAT: add discorver page

---
+++ chamber/scripts/lastVisit.js
@@ -0,0 +1,32 @@
+
+let currentDate = new Date()
+var lastVisit = window.localStorage.getItem("lastVisit")
+if (lastVisit == null) {
+    updateDaysH3(0)
+} else {
+    lastDate = new Date(lastVisit)
+    let days = dayDiff(lastDate, currentDate)
+    updateDaysH3(days)
+}
+
+
+function updateDaysH3(days) {
+    var differenceString = ""
+    if (days == 0) {
+        differenceString = "today"
+    } else if (days == 01) {
+        differenceString = "yesterday"
+    } else {
+        differenceString = `${days} days ago`
+    }
+    var daysH3 = document.querySelector("#daysBetweenLast")
+    daysH3.textContent = differenceString
+    localStorage.setItem("lastVisit", currentDate)
+}
+
+function dayDiff(pastDate, currentDate) {
+    diff = currentDate.getTime() - pastDate.getTime()
+    days = Math.trunc(diff / (1000 * 3600 * 24))
+    console.log(days)
+    return days
+}
