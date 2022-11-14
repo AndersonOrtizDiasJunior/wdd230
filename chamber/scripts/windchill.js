39a76b65db057ef2a7a5006c65a4568da3c16b16
AndersonOrtizDiasJunior <anderson_ortiz@hotmail.com.br>
2022-11-14 18:43:34 -0300

    FEAT: weather api on home

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,14 +1,15 @@
-chillLabel = document.querySelector("#windChill")
-speed = Number(document.querySelector("#windSpeed").textContent)
-temperature = Number(document.querySelector("#temperatureValue").textContent)
-if (isChillCalculable(temperature, speed)) {
-    chillLabel.textContent = calculateWindChill(temperature, speed)}
-
-chillLabel = document.querySelector("#windChillLarge")
-speed = Number(document.querySelector("#windSpeedLarge").textContent)
-temperature = Number(document.querySelector("#temperatureValueLarge").textContent)
-if (isChillCalculable(temperature, speed)) {
-    chillLabel.textContent = calculateWindChill(temperature, speed)}
+chillLabel = document.querySelectorAll(".windChill")
+speed = Number(document.querySelector(".windSpeed").textContent)
+temperature = Number(document.querySelector(".temperatureValue").textContent)
+speed.addEventListener('update', displayWindchill)
+temperature.addEventListener('update', displayWindchill)
+function displayWindchill() {
+    if (isChillCalculable(temperature, speed)) {
+        chillLabel.array.forEach(label => {
+            label.textContent = calculateWindChill(temperature, speed)
+        });
+    }
+}
 
 function calculateWindChill(temperature, speed) {
     chill = 35.74 + 0.6215 * temperature - 35.75 * speed ** 0.16 + 0.4275* temperature * speed ** 0.16
